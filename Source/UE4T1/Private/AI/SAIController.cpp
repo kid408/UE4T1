8ca57d3674f5f58ef700bf1fb515695d475de587
kid408 <kid_408@163.com>
2023-06-24 19:32:34 +0800

    add ai

---
+++ Source/UE4T1/Private/AI/SAIController.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SAIController.h"
+#include <Kismet/GameplayStatics.h>
+#include "BehaviorTree/BlackboardComponent.h"
+
+void ASAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	RunBehaviorTree(BehaviorTree);
+
+	APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	if (MyPawn)
+	{
+		GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
+		GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
+	}
+}
---
+++ Source/UE4T1/Private/AI/SBTTask_RangedAttack.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTTask_RangedAttack.h"
+#include <SCharacter.h>
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type USBTTask_RangedAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* MyController = OwnerComp.GetAIOwner();
+	if (ensure(MyController))
+	{
+		// 注意，是ACharacter ，而不是 ASCharacter
+		ACharacter* MyPawn = Cast<ACharacter>(MyController->GetPawn());
+		if (MyPawn == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		FVector MuzzleLocation = MyPawn->GetMesh()->GetSocketLocation("Muzzle_01");
+
+		AActor* TargetActor = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("TargetActor"));
+		if (TargetActor == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		if (!OwnerComp.GetBlackboardComponent()->GetValueAsBool("WithinAttackRange"))
+		{
+			return EBTNodeResult::Failed;
+		}
+		
+
+		FVector Direction = TargetActor->GetActorLocation() - MuzzleLocation;
+		FRotator MuzzleRotation = Direction.Rotation();
+
+		FActorSpawnParameters Params;
+		Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		AActor* NewProj = GetWorld()->SpawnActor<AActor>(ProjectileClass, MuzzleLocation, MuzzleRotation, Params);
+		return NewProj ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+	}
+
+	return EBTNodeResult::Failed;
+}
